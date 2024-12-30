893b04a0432d02d89fdaf0cc1486426597410584
Kundan Gope <kundan.gope@mapmyindia.com>
2024-12-30 14:42:11 +0530

    removed redis import

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import routes from "./routes/index.js";
 import errorHandler from "./middlewares/errorHandler.js";
-import redisClient from "./db/redisInstance.js";
 
 const app = express();
 
